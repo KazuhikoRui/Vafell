010cba6939c824e55236b64507025dfc99afeebd
KazuhikoRui <70285464+KazuhikoRui@users.noreply.github.com>
2021-02-05 19:28:05 +0700

    Update Vafell.py

--- Vafell.py
+++ Vafell.py
@@ -1,36 +1,36 @@
 import amino
-import random
-import re
-import datetime
 import os
 
 mail = os.environ.get('V_NAME')
 passw = os.environ.get('V_PASS')
+
+
 client = amino.Client()
 client.login(email=mail, password=passw) 
 sub_client = amino.SubClient(comId='156542274', profile=client.profile) 
-
+print('Bot - on')
 
 def on_message(data):
-	chatId = data.message.chatId
-	nickname = data.message.author.nickname
-	content = data.message.content
-	vrem = data.message.createdTime[17:19]
-	id = data.message.messageId
-	if data.message.type != 0:
-		print(f"{nickname}: {content}: {chatId}") 
-	
-	content = str(content).split(" ")
-	if content[0][0] == "!" and content[0][1:].lower() == "ку":
-		sub_client.send_message(message="Дарова, отец...", chatId=chatId)
-	##################################Защита чата##################################################
-
-	if data.message.content != None and data.message.type in [1, 50, 58, 57, 59, 100, 101, 102, 103, 104, 105, 106, 107, 109, 110, 113, 114, 115, 116, 124, 125, 126]:
-		sub_client.send_message(message='Не тот чатик, малыш :)', chatId=data.message.chatId)
-		sub_client.delete_message(chatId=data.message.chatId, messageId=data.message.messageId)
-		sub_client.kick(userId=data.message.author.userId, chatId=data.message.chatId, allowRejoin = True)
-
-
+    chatid = data.message.chatId
+    nickname = data.message.author.nickname
+    content = data.message.content
+    mtype = data.message.type
+    chatname = subclient.get_chat_thread(chatId=data.message.chatId).title
+    mid = data.message.messageId
+    uid = data.message.author.userId
+    print(f'{chatname}: {nickname}: {mtype}: {content}')
+    
+    content = str(content).split(" ")
+    if content[0][0] == "!" and content[0][1:].lower() == "ку":
+        sub_client.send_message(message="Дарова, отец...", chatId=chatId)
+    
+    if (mtype == 100) | (mtype == 109) | (mtype == 107)  | (mtype == 110) | (mtype == 108)  | (mtype == 111) | (mtype == 111):
+        if mtype == 100 and content == None:
+                  pass
+        else:
+            subclient.kick(chatId=chatid, userId = uid, allowRejoin=False)
+            subclient.send_message(chatId = chatid, message = 'Не тот чатик, малыш')
 methods = []
 for x in client.callbacks.chat_methods:
-	methods.append(client.callbacks.event(client.callbacks.chat_methods[x].__name__)(on_message))
+    methods.append(client.callbacks.event(client.callbacks.chat_methods[x].__name__)(on_message))
+
